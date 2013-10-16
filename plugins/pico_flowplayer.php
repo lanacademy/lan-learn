e8e9d2cf01ba664ad6d2aa187f6dda6527390e23
BenOverholts <commandershadow@gmail.com>
2013-10-16 00:46:03 -0400

    Didn't upload plugin file, oops

---
+++ plugins/pico_flowplayer.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * A plugin that uses flowplayer to embed videos referenced in markdown
+ *
+ * @author Ben Overholts
+ * @link http://www.benoverholts.com/
+ * @license http://opensource.org/licenses/MIT
+ */
+class Pico_Flowplayer {
+
+	public function __construct() {
+		$plugin_path = dirname(__FILE__);
+		$this->path = $plugin_path;
+	}
+
+	public function before_read_file_meta(&$headers)
+	{
+		$headers['subject'] = 'Subject';
+	}
+
+	public function config_loaded(&$settings) {
+		$this->theme = $settings['theme'];
+	}
+
+	public function file_meta(&$meta)
+	{
+		$this->subject = $meta['subject'];
+	}
+
+	public function before_load_content(&$file) {
+
+		if (file_exists($file)) {
+			$this->lines = explode("\n", file_get_contents($file));
+		}
+	}
+
+	public function content_parsed(&$content)
+	{
+		$videotitle = "";
+		$content = "";
+		for($i = 0; $i < count($this->lines); ++$i) {
+			if (preg_match("/^!!/", $this->lines[$i])) {
+    			$videotitle = $this->lines[$i];
+    			$this->videopath = str_replace(" ", "_", strtolower($this->subject));
+				$this->videopath = "/content/" . $this->videopath . "/media/";
+				$videotitle = str_replace("!!", "", $videotitle);
+				$this->videopath = $this->videopath . $videotitle;
+
+
+				$content = $content . $_SERVER['DOCUMENT_ROOT'] . $this->videopath;
+				$content = $content .  '<div class="flowplayer">
+   											<video>
+      											<source type="video/mp4" src="' . $_SERVER['DOCUMENT_ROOT'] . $this->videopath . '">
+   											</video>
+										</div>';
+
+			} else {
+				$content = $content . $this->lines[$i] . "<br>";
+			}
+		}
+	}
+
+	public function get_page_data(&$data, $page_meta) {
+
+	}
+}
